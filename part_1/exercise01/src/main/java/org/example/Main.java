03d651cede7d9df49884e0ecd18b5ca785ab489d
GriffBlack <giriffblack42018@gmail.com>
2025-05-30 11:14:54 +0300

    ex1 done

--- part_1/exercise01/src/main/java/org/example/Main.java
+++ part_1/exercise01/src/main/java/org/example/Main.java
@@ -1,17 +1,55 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        double[] coords = new double[6];
+        String[] prompts = {
+                "Enter x1: ", "Enter y1: ",
+                "Enter x2: ", "Enter y2: ",
+                "Enter x3: ", "Enter y3: "
+        };;
+        System.out.println("Введите координаты вершин треугольника:");
+        for (int i = 0; i < 6; ) {
+            System.out.print(prompts[i]);
+            try {
+                coords[i] = Double.parseDouble(scanner.nextLine());
+                i++;
+            } catch (NumberFormatException e) {
+                System.out.println("Couldn't parse a number. Please, try again");
+            }
         }
+        double[] sides = calculateSides(coords[0], coords[1], coords[2], coords[3], coords[4], coords[5]);
+        if (isTriangle(sides[0], sides[1], sides[2])){
+            double perimeter = calculatePerimeter(sides[0], sides[1], sides[2]);
+            System.out.printf("Triangle perimeter: %.3f%n", perimeter);
+        } else  {
+            System.out.println("It isn't triangle");
+        }
+
+    }
+    public static double calculatePerimeter(double a, double b, double c) {
+        return a + b + c;
+    }
+    public static boolean isTriangle(double a, double b, double c) {
+        return a > 0 && b > 0 && c > 0 &&
+                a + b > c + 1e-10 &&
+                a + c > b + 1e-10 &&
+                b + c > a + 1e-10;
+    }
+
+    public static double calculateDistance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
+    private static double[] calculateSides(double x1, double y1,
+                                           double x2, double y2,
+                                           double x3, double y3) {
+        double a = calculateDistance(x1, y1, x2, y2);
+        double b = calculateDistance(x2, y2, x3, y3);
+        double c = calculateDistance(x1, y1, x3, y3);
+        return new double[]{a, b, c};
     }
 }
