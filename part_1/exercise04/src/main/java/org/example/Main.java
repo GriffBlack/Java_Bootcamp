df32fc86f2299f0929d0a876c769a2f1b1f4475a
GriffBlack <giriffblack42018@gmail.com>
2025-06-03 21:28:39 +0300

    ex4 final

--- part_1/exercise04/src/main/java/org/example/Main.java
+++ part_1/exercise04/src/main/java/org/example/Main.java
@@ -1,37 +1,51 @@
 package org.example;
 
+import java.time.LocalTime;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    private int numberOfDigits = 0;
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter muss number");
         while(true) {
             try {
+                System.out.println("Enter the array size");
                 int numberOfDigits = scan.nextInt();
-                printResult(numberOfDigits);
+                if (numberOfDigits <= 0) {
+                    throw new InputMismatchException("Input error. Size <= 0");
+                }
+                int sumDigits = parser(scan, numberOfDigits);
+                printResult(sumDigits);
                 break;
             } catch (InputMismatchException e) {
+                System.out.println(e.getMessage() != null ? e.getMessage() : "");
                 System.out.println("Couldn't parse a number. Please, try again");
             }
-            parser(args);
+            scan.nextLine();
         }
-    }
-    private static int parser(String[] args) {
-
-    }
-    private static void printResult(int numberOfDigits) {
-        System.out.println(numberOfDigits);
+        System.out.println(scan.nextInt());
+        scan.close();
     }
 
-    public void setNumberOfDigits(int numberOfDigits) {
-        this.numberOfDigits = numberOfDigits;
+    private static int parser(Scanner scan, int numberOfDigits) {
+        int sum = 0;
+        int[] array = new int[numberOfDigits];
+        int colDigits = 0;
+        try {
+            for (int j = 0; j < numberOfDigits; j++) {
+                array[j] = scan.nextInt();
+                if (array[j] < 0) {
+                    sum -= array[j];
+                    colDigits++;
+                }
+            }
+        } catch (Exception e) {
+            throw new InputMismatchException();
+        }
+        return colDigits > 0 ? sum/colDigits : -1;
+    }
+    private static void printResult(int sumDigits) {
+        System.out.println( sumDigits == -1 ? "There are no negative elements" : sumDigits);
     }
 
-    public int getNumberOfDigits() {
-        return numberOfDigits;
-    }
 }
